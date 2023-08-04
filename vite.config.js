87b72ddf3e95583a2b3de849b92fed428cb06a77
CodeWithChirag14 <chiragnankani15@gmail.com>
2023-08-04 08:44:01 +0530

    update build for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Crud_Operations",
+  base: "/Crud_Operations/",
   plugins: [react()],
 })
